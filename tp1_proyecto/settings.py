2064fd204780a44b869aa93f847a8a8f89038d5c
Gabriel1700 <gabrielsanchezacuna@gmail.com>
2026-08-29 02:34:45 -0600

    Configuracion de seguridad para las credenciales de la BD, utilizando variables de entorno

--- tp1_proyecto/settings.py
+++ tp1_proyecto/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,10 +75,19 @@ WSGI_APPLICATION = 'tp1_proyecto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mssql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),                         
+        'OPTIONS': {
+            'driver': 'ODBC Driver 18 for SQL Server', 
+            'extra_params': 'TrustServerCertificate=yes',
+        },
     }
 }
+
 
 
 # Password validation
