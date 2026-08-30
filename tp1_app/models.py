e5b21f370b17ca20e93fd4dc9317e0d17f4c4d53
Gabriel1700 <gabrielsanchezacuna@gmail.com>
2026-08-29 21:46:31 -0600

    Implementación de templates, nuevos archivos .html para la pagina de lista de empleados y la página de insertar, así como la creación del modelo Empleado en models.py

--- tp1_app/models.py
+++ tp1_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Empleado(models.Model):
+    nombre = models.CharField(db_column='Nombre', max_length=128, db_collation='SQL_Latin1_General_CP1_CI_AS')  # Field name made lowercase.
+    salario = models.DecimalField(db_column='Salario', max_digits=19, decimal_places=4)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'Empleado'
--- tp1_app/views.py
+++ tp1_app/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Empleado
 
 # Create your views here.
 
 def empleado(request):
-    return HttpResponse("Lista de empleados")
+    lista_empleados = Empleado.objects.all().order_by('nombre')
+
+    template = loader.get_template('empleados.html')
+
+    context = {
+        'empleados': lista_empleados,
+    }
+
+    return HttpResponse(template.render(context, request))
 
 def insertar_empleado(request):
-    return HttpResponse("Insertar empleados")
+    template = loader.get_template('insertar_empleado.html')
+    return HttpResponse(template.render())
